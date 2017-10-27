41f3838205e881b6a0110ab28207bb095ab807ec
etudiant <etudiant@iMac-de-etudiant.local>
2017-10-27 10:47:17 +0200

    Initial Commit

---
+++ BattleRoyal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BattleRoyal
+//
+//  Created by etudiant on 27/10/2017.
+//  Copyright © 2017 etudiant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
