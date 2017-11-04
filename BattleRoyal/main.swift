c447dbffd70af41f68a776758ac40aeed8ffe174
caroline <caroline@MacBook-Pro-de-caroline.local>
2017-11-04 21:39:41 +0100

    the best half

--- BattleRoyal/main.swift
+++ BattleRoyal/main.swift
@@ -13,12 +13,24 @@ func updatePlayers (players: [Character]) -> [Character] {
     return players.filter { $0.health > 0 }
 }
 
+func displayPlayers(players: [Character]) {
+    var i = 0
+    players.forEach { charac in
+        i += 1
+        if i == (players.count) {
+            print("et", terminator: " ")
+        }
+        print(charac.name, terminator: " ")
+    }
+}
+
 // players initialization
 let noob = Character(name: "kékédu93")
 let steve = Character(name: "Steve", weapon: Sword(name: "Dard", height: 50.0, width: 5.0, weight: 5.0, price: 70.0, year: 50))
 let roiArthur = Character(name: "roi Arthur", weapon: Excalibur())
 let froster = Character(name: "Froster", weapon: Gun(name: "Diplomatie", height: 7.0, width: 2.0, weight: 5.0, price: 1000.0, bullets: 10))
 var players = [noob, steve, roiArthur, froster]
+let halfPlayers = players.count / 2
 
 print("La bataille commence !")
 var i = 1
@@ -31,7 +43,8 @@ repeat {
         }
         players = updatePlayers(players: players)
     })
-} while (players.count > 1)
+} while (players.count > halfPlayers)
 
-print("\nEt le gagnant est \(players[0].name) !")
+print("\nEt les gagnants sont :")
+displayPlayers(players: players)
 
